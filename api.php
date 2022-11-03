8125d4d5c4a363ad5fcba42aa28634436cd47834
LuisalbertosanchezmarinADSI <luissanchezm1019@gmail.com>
2022-11-03 17:57:25 -0500

    Ejercicio 3

--- api.php
+++ api.php
@@ -1,36 +1,60 @@
 <?php
-    header('Access-Control-Allow-Origin: *');
+    header("Access-Control-Allow-Origin: *");
     header('Content-Type: application/json; charset=utf-8');
     $_DATA = json_decode(file_get_contents("php://input"),true);
-    class factura{
-        public $codigo;
-        public $unidades;
-        
-        public function __construct(int $codigo, int $unidades){
-            $this->codigo = $codigo;
-            $this->unidades = $unidades;
+
+    class promedio{
+        public $sumaA;
+        public $canA;
+        public $sumaB;
+        public $canB;
+        public $sumaC;
+        public $canC;
+
+        public function __construct(int $sumaA,int $canA,int $sumaB,int $canB, int $sumaC,int $canC){
+            $this->sumaA = $sumaA;
+            $this->canA = $canA;
+            $this->sumaB = $sumaB;
+            $this->canB = $canB;
+            $this->sumaC = $sumaC;
+            $this->canC = $canC;
         }
 
+        public function promedioF():string{
 
-        public function numerocodigo(){
-            $codigo=match($this->codigo){
-                1=> $codigo =20400,
-                2=> $codigo =34896,
-                3=>$codigo =37850,
-                4=>$codigo =40345,
-                default=>$codigo="Codigo Incorrecto"
-            };
-            return $codigo;
-        }
+            $sumaA = $this->sumaA;
+            $canA = $this->canA;
+            $sumaB = $this->sumaB;
+            $canB = $this->canB;
+            $sumaC = $this->sumaC;
+            $canC = $this->canC;
 
-        public function totalpagar():string{
-            $codigo=$this->numerocodigo();
-            $totalpagar=$codigo*$this->unidades;
-            return $totalpagar;
+            $canA<1?$proA=0:$proA = $sumaA/$canA;
+            $canB<1?$proB=0:$proB = $sumaB/$canB;
+            $canC<1?$proC=0:$proC = $sumaC/$canC;
+
+            if($proA>$proB && $proA>$proC){
+                if($proB>$proC){
+                    return "C";
+                } else{
+                    return "B";
+                }
+            } else if($proB>$proA && $proB>$proC){
+                if($proA>$proC){
+                    return "C";
+                } else{
+                    return "A";
+                }
+            } else if($proC>$proA && $proC>$proB){
+                if($proA>$proB){
+                    return "B";
+                } else{
+                    return "A";
+                }
+            } else{
+                return "A, B, C";
+            }
         }
     }
-        
-    $listado=new factura(codigo:$_DATA['codigo'],unidades:$_DATA['unidades']);
-    echo ($listado->totalpagar());
-
-?>
+    $lista = new promedio(sumaA:$_DATA[0],canA:$_DATA[1],sumaB:$_DATA[2],canB:$_DATA[3],sumaC:$_DATA[4],canC:$_DATA[5]);
+    echo($lista->promedioF());
--- main.js
+++ main.js
@@ -1,15 +1,80 @@
-addEventListener("DOMContentLoaded", (e)=>{
-    let myForm = document.querySelector("#Formulario");
-    myForm.addEventListener("submit", async(e)=>{
+let sumaA=0,sumaB=0,sumaC=0,canA=0,canB=0,canC=0;
+
+function sumaPro(genero, carrera, edad) {
+    if(genero=="masculino"){
+        if(carrera=="a"){
+            sumaA= sumaA+edad;
+            canA +=1;
+        }else if (carrera=="b"){
+            sumaB= sumaB+edad;
+            canB +=1;
+        }else{
+            sumaC= sumaC+edad;
+            canC +=1;
+        }
+    } else{
+        null
+    }
+}
+
+addEventListener("DOMContentLoaded", (e) => {
+    document.querySelector("#cantidad").removeAttribute("disabled");
+    document.querySelector("#cantidad").addEventListener("keyup", (e) => {
+        let genero = document.querySelector("#genero");
+        let edad = document.querySelector("#edad");
+        let carrera = document.querySelector("#carrera");
+        if (e.target.value > 0) {
+            genero.removeAttribute("disabled");
+            edad.removeAttribute("disabled");
+            carrera.removeAttribute("disabled");
+        } else {
+            genero.disabled = true;
+            edad.disabled = true;
+            carrera.disabled = true;
+        }
+    })
+    let con = 1;
+    let lista = [];
+    let myform = document.querySelector("#form");
+    myform.addEventListener("submit", async (e) => {
         e.preventDefault();
-        let Formulario = e.target;
-        let data = Object.fromEntries(new FormData(Formulario));
-        let config = {
-            method :Formulario.method,
-            body: JSON.stringify(data)
-        };
-        let peticion = await fetch(Formulario.action,config);
-        let resultado = await peticion.text();
-        document.querySelector("#resultado").insertAdjacentElement ("beforeend",resultado);
+        let cantidad = document.querySelector("#cantidad").value;
+        document.querySelector("#cantidad").disabled = true;
+        if (con < cantidad) {
+            let data = Object.fromEntries(new FormData(form));
+            sumaPro(data["genero"], data["carrera"], parseInt(data["edad"]));
+            document.querySelector("#genero").selectedIndex = 0;
+            document.querySelector("#edad").value = "";
+            document.querySelector("#carrera").selectedIndex = 0;
+            con += 1;
+
+        } else {
+            let form = e.target;
+            let data = Object.fromEntries(new FormData(form));
+            sumaPro(data["genero"], data["carrera"], parseInt(data["edad"]));
+            document.querySelector("#res").innerHTML = "";
+            lista.push(sumaA);
+            lista.push(canA);
+            lista.push(sumaB);
+            lista.push(canB);
+            lista.push(sumaC);
+            lista.push(canC);
+            
+            let config = {
+                method: form.method,
+                body: JSON.stringify(lista)
+            };
+
+            let peticion = await fetch(form.action, config);
+            let respuesta = await peticion.text();
+            document.querySelector("#res").insertAdjacentHTML("beforeend", respuesta);
+            document.querySelector("#genero").selectedIndex = 0;
+            document.querySelector("#edad").value = "";
+            document.querySelector("#carrera").selectedIndex = 0;
+            genero.disabled = true;
+            edad.disabled = true;
+            carrera.disabled = true;
+        }
+
     })
 })
